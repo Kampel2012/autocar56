b91144961ee0287849b9d93452d00b50106665c1
Anton Glazunov <kampel2012@yandex.ru>
2023-06-21 22:33:14 +0500

    upd

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/autocar56/',
   plugins: [react()],
-  build: {
-    outDir: './build',
-  },
 });
